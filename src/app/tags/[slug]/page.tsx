a664dc35f0543b2744c2dff5f75331f74a19b6c5
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2024-07-02 04:45:56 +0600

    client folder removed

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -1,9 +1,13 @@
 import { getPostsByTag, getTag } from '@/lib/api'
 import React from 'react'
 import Posts from "@/components/Posts";
+import Error from '@/app/error';
 
 export default async function page({ params: { slug } }: { params: { slug: string } }) {
-    const tag: TagType = await getTag(slug)
+    const tag: TagType | null = await getTag(slug)
+
+    if (!tag) return <Error error={{ message: "Requrested Tag does not exist!", name: 'tag_error' }} />
+
     const posts: PostType[] = await getPostsByTag(tag._id)
 
     return (
